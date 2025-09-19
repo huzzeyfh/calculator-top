4d93c4b66f3a70822d884669e8d8635c3c868eaa
huzzeyfh <hudzaifah7103@gmail.com>
2025-09-19 17:19:27 +0700

    Add clear function to button 'C'
    Still wondering how to functioning 'backspace'

--- script.js
+++ script.js
@@ -22,11 +22,13 @@ function divide(a, b) {
 //Variables
 const container = document.querySelector('.container');
 const button = document.querySelectorAll('button')
+const backspaceButton = document.querySelector('.btn-backspc');
+const clearButton = document.querySelector('.btn-clear');
 // const firstNumber;
 // const operator;
 // const secondNumber;
 const input = document.querySelector('#input');
-
+let currentDisplay = input.textContent;
 
 //Operate
 function operate(firstNumber, operator, secondNumber) {
@@ -36,8 +38,25 @@ function operate(firstNumber, operator, secondNumber) {
 
 //Display input
 container.addEventListener('click', (e) => {
+    let target = e.target;
+    
+    if (target.tagName != 'BUTTON') return;
+
     if (input.textContent == 0) {
-        input.textContent = '';
+       input.textContent = '';
     }
+
+    if (e.target.classList.contains('btn-backspc')) {
+        return currentDisplay.slice(0, -1);
+    }
+
+    if (e.target.classList.contains('btn-clear')) {
+        input.textContent = 0;
+        return ;
+    }
+
     input.textContent += e.target.textContent;
+    console.log(input.textContent);
+    return ;
 })
+
