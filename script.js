91e69948a519194a41bd296740887b7b772ba005
huzzeyfh <hudzaifah7103@gmail.com>
2025-09-19 16:35:15 +0700

    Add calculator display
    Add event to insert clicked button to display

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //Add 
 function add(a, b) {
     return a + b;
-} 
+}
 
 //Subtract
 function subtract(a, b) {
@@ -20,9 +20,12 @@ function divide(a, b) {
 
 
 //Variables
-const firstNumber;
-const operator;
-const secondNumber;
+const container = document.querySelector('.container');
+const button = document.querySelectorAll('button')
+// const firstNumber;
+// const operator;
+// const secondNumber;
+const input = document.querySelector('#input');
 
 
 //Operate
@@ -30,3 +33,11 @@ function operate(firstNumber, operator, secondNumber) {
     return add(firstNumber, secondNumber)
 }
 
+
+//Display input
+container.addEventListener('click', (e) => {
+    if (input.textContent == 0) {
+        input.textContent = '';
+    }
+    input.textContent += e.target.textContent;
+})
